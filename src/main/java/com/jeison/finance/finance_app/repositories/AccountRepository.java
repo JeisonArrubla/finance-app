b40c3cdbd3b18f135470f18203a49e6f7c213876
Jeison <jeison.arrubla@gmail.com>
2024-06-27 16:28:16 -0500

    logica para evitar que se guarden accounts con user null o description repetidas por user

--- src/main/java/com/jeison/finance/finance_app/repositories/AccountRepository.java
+++ src/main/java/com/jeison/finance/finance_app/repositories/AccountRepository.java
@@ -1,9 +1,13 @@
 package com.jeison.finance.finance_app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeison.finance.finance_app.models.Account;
+import com.jeison.finance.finance_app.models.User;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
+    Optional<Account> findByDescriptionAndUser(String description, User user);
 }
--- src/main/java/com/jeison/finance/finance_app/services/AccountService.java
+++ src/main/java/com/jeison/finance/finance_app/services/AccountService.java
@@ -5,18 +5,34 @@ import org.springframework.stereotype.Service;
 
 import com.jeison.finance.finance_app.models.Account;
 import com.jeison.finance.finance_app.repositories.AccountRepository;
+import com.jeison.finance.finance_app.repositories.UserRepository;
 
 @Service
 public class AccountService {
 
     @Autowired
-    private AccountRepository repository;
+    private AccountRepository accountRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public Account saveAccount(Account account) {
         Account a = new Account();
         a.setDescription(account.getDescription());
         a.setBalance(account.getBalance());
-        a.setUser(account.getUser());
-        return repository.save(a);
+        // Validar si el usuario existe
+        userRepository.findById(account.getUser().getId())
+                .ifPresentOrElse(
+                        a::setUser,
+                        () -> a.setUser(null));
+        if (a.getUser() == null) {
+            System.err.println("No se encontró el usuario");
+            return null;
+        }
+        // Validar que el usuario no tenga una cuenta con la misma descripción
+        if (accountRepository.findByDescriptionAndUser(account.getDescription(), account.getUser()).isPresent()) {
+            System.err.println("Nombre de cuenta ya existe");
+            return null;
+        }
+        return accountRepository.save(a);
     }
 }
