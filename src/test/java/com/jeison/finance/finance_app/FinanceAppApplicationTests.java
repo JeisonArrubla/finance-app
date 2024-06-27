b16465c4fb6fcfcdee032ea95b7a90de3d3f2d51
Jeison <jeison.arrubla@gmail.com>
2024-06-27 01:19:31 -0500

    Primer commit

---
+++ src/test/java/com/jeison/finance/finance_app/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeison.finance.finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
