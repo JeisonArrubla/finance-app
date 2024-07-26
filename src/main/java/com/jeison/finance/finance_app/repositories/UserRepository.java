a11978884a21d467578700efb849a1cf0b3ed70f
Jeison <jeison.arrubla@gmail.com>
2024-07-26 02:50:09 -0500

    En UserRepository se crea el metodo findByUsername. Se implementa UserDetailsService con jpa en la clase JpaUserDetailsService.

--- src/main/java/com/jeison/finance/finance_app/repositories/UserRepository.java
+++ src/main/java/com/jeison/finance/finance_app/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.jeison.finance.finance_app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeison.finance.finance_app.models.User;
@@ -7,4 +9,6 @@ import com.jeison.finance.finance_app.models.User;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/jeison/finance/finance_app/services/JpaUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.jeison.finance.finance_app.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jeison.finance.finance_app.models.User;
+import com.jeison.finance.finance_app.repositories.UserRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException(String.format("El usuario %s no está registrado", username));
+        }
+        User user = userOptional.orElseThrow();
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                authorities);
+    }
+}
