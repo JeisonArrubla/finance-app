520a088f6d968712a4409ee0e10a8cee1b6f2935
Jeison <jeison.arrubla@gmail.com>
2024-08-02 00:55:21 -0500

    Se crean usuarios role user y role admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO finance_db.roles (name) VALUES ('ROLE_ADMIN');
 INSERT INTO finance_db.roles (name) VALUES ('ROLE_USER');
 
-INSERT INTO finance_db.users (enabled, id, username, password) VALUES (1, 1, "happy_dog", "$2a$10$C1.XbN79T/O9D6F18tLREOgw.PSl3u7cGa7mBoObb2NAJByCXs49C");
-INSERT INTO finance_db.users (enabled, id, username, password) VALUES (1, 2, "coffee-man", "$2a$10$C1.XbN79T/O9D6F18tLREOgw.PSl3u7cGa7mBoObb2NAJByCXs49C");
+INSERT INTO finance_db.users (enabled, id, username, password) VALUES (1, 1, "user", "$2a$10$C1.XbN79T/O9D6F18tLREOgw.PSl3u7cGa7mBoObb2NAJByCXs49C");
+INSERT INTO finance_db.users (enabled, id, username, password) VALUES (1, 2, "admin", "$2a$10$C1.XbN79T/O9D6F18tLREOgw.PSl3u7cGa7mBoObb2NAJByCXs49C");
 
 INSERT INTO finance_db.users_roles (role_id, user_id) VALUES (2, 1);
 INSERT INTO finance_db.users_roles (role_id, user_id) VALUES (2, 2);
